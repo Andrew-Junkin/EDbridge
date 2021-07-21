7f1d7b087f748fef361fb6231c3a910cc10162ec
sg0703 <samgates@Sams-MBP.attlocal.net>
2021-07-21 12:10:06 -0400

    Structure of frontend complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
-import {
-        Menu,
-        Button,
-        Form,
-        Input
-       } from 'semantic-ui-react'
+import React from "react";
+import { Router, Route, Switch } from 'react-router-dom'; 
+import { ApolloProvider } from '@apollo/client';
+import { client } from './config/apollo';
 
+// history
+import history from './config/history';
 
+// bring in pages
+import Home from "./pages/Home";
 
-export default class MenuExampleHeader extends Component {
-  state = {}
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
-  render() {
-    const { activeItem } = this.state
-//Make sign in conditional on existing sessions
-    return (
-            
-      <Menu inverted >
-        <Menu.Item header as='h3' >Edify Bridge</Menu.Item>
-        <Menu.Item
-          name='Classes'
-          active={activeItem === 'Classes'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='Tutors'
-          active={activeItem === 'Tutors'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='Contact us'
-          active={activeItem === 'Contact us'}
-          onClick={this.handleItemClick}
-        />
-        
-        <Menu.Item
-           
-          name='Sign in'
-          active={activeItem === 'Sign in'}
-          onClick={this.handleItemClick}
-        />
-      </Menu>
-      
-     
-             
-                
-    )
-  }
- }
- class MyComponent extends React.Component {
-  render() {
-    return (
-      <Form>
-        <Form.Field>
-          <label htmlFor="name">Name</label>
-          <Input name="name" />
-        </Form.Field>
-        <Form.Field>
-          <label htmlFor="email">Email</label>
-          <Input name="email" />
-        </Form.Field>
-        <Button primary>Submit</Button>
-      </Form>
-    );
-  }
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
-
-
+export default App;
